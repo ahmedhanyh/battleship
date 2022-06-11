1e981642849eb05b61c613def17d3e4064bc5b09
Ahmed Hany <ahmedhanyhosny@outlook.com>
2022-06-11 15:25:22 +0200

    Create 'Player' factory and test it

---
+++ test/player.test.js
@@ -0,0 +1,21 @@
+import Player from "../lib/player";
+
+it("Player(name) creates a new player with the name passed in", () => {
+  const player = Player("Player");
+  expect(player.gameboard).not.toBeNull();
+  expect(player.name).toBe("Player");
+});
+
+it("Player().attack(opponent, coords) attacks the opponent's ship at specified coords", () => {
+  const player = Player("Player");
+  const opponent = Player("Computer");
+  player.attack(opponent, "1A");
+  expect(opponent.gameboard.board[0]["A"]).toBe("missed");
+});
+
+it("Player().attack(opponent, coords) attacks the opponent's ship at specified coords", () => {
+  const player = Player("Player");
+  const opponent = Player("Computer");
+  player.attack(opponent, "1B");
+  expect(opponent.gameboard.board[0]["B"]).toBe("hit");
+});
